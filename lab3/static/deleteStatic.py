f39f7f688c0d7df9ef27dbbab3f2f8e91424df77
DenisRudiy <denryd2311@gmail.com>
2024-04-07 16:20:14 +0300

    l3

--- lab3/static/deleteStatic.py
+++ lab3/static/deleteStatic.py
@@ -17,48 +17,28 @@ def delete_student(student_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute('DELETE FROM students WHERE id = %s;', (student_id,))
-    cursor.execute('CALL update_std_ids(%s);', (student_id,))
-    cursor.execute("SELECT MAX(id) FROM students")
-    max_id = cursor.fetchone()[0]
-    cursor.execute('ALTER SEQUENCE students_id_seq RESTART WITH %s;', (max_id+1,))
     db.commit()
 
 def delete_group(group_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute('DELETE FROM groups WHERE id = %s;', (group_id,))
-    cursor.execute('CALL update_group_ids(%s);', (group_id,))
-    cursor.execute("SELECT MAX(id) FROM groups")
-    max_id = cursor.fetchone()[0]
-    cursor.execute('ALTER SEQUENCE groups_id_seq RESTART WITH %s;', (max_id+1,))
     db.commit()
 
 def delete_subject(subject_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute('DELETE FROM subjects WHERE id = %s;', (subject_id,))
-    cursor.execute('CALL update_subj_ids(%s);', (subject_id,))
-    cursor.execute("SELECT MAX(id) FROM subjects")
-    max_id = cursor.fetchone()[0]
-    cursor.execute('ALTER SEQUENCE subjects_id_seq RESTART WITH %s;', (max_id+1,))
     db.commit()
 
 def delete_subject_type(subject_type_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute('DELETE FROM subj_types WHERE id = %s;', (subject_type_id,))
-    cursor.execute('CALL update_subj_type_ids(%s);', (subject_type_id,))
-    cursor.execute("SELECT MAX(id) FROM subj_types")
-    max_id = cursor.fetchone()[0]
-    cursor.execute('ALTER SEQUENCE subj_types_id_seq RESTART WITH %s;', (max_id+1,))
     db.commit()
 
 def delete_grade(grade_id):
     db = get_db()
     cursor = db.cursor()
     cursor.execute('DELETE FROM grades WHERE id = %s;', (grade_id,))
-    cursor.execute('CALL update_grade_ids(%s);', (grade_id,))
-    cursor.execute("SELECT MAX(id) FROM grades")
-    max_id = cursor.fetchone()[0]
-    cursor.execute('ALTER SEQUENCE grades_id_seq RESTART WITH %s;', (max_id+1,))
     db.commit()
